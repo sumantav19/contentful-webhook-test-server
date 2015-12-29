03cfd1922d3f01568cd15f723fdc8b189fbe6c4e
= <=>
2015-12-29 14:46:19 +0530

    Added route to index.html and gulp task for jade

--- routes/contentful-webhook.js
+++ routes/contentful-webhook.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var gulp = require('gulp'), gulpjade = require('gulp-jade') , gulpData = require('gulp-data');
 
 router.get('/', function(req, res, next) {
   res.status(200).send('Reached to webhook');
@@ -8,9 +9,20 @@ router.get('/', function(req, res, next) {
 
 router.post('/',function(request,response){
 	console.log(request.body);
-	//response.status(200).send(request.header);
+	
 	if(request.headers['x-contentful-topic'] && request.headers['x-contentful-topic'] === 'ContentManagement.Entry.publish'){
 		
+		gulp.task('templates',function(){
+			console.log('task Running');
+			gulp.src(['../src/jade/*.jade'])
+				.pipe(gulpjade(function(file){
+					return request.body;
+				}))
+				.pipe(gulpjade( {pretty: true}))
+				.pipe(gulp.dest('../public/'));
+		})
+		gulp.start('templates');
+
 		fs.writeFile('./test-file-folder/test-file.txt',request.body || 'test for content ful',function(err){
 			if(err){
 				return response.status(500).send('could not write file');
